e904396e2cbe547285431485787807e873c85658
Dušan Uverić <dusan.uveric@makit.lv>
2022-03-30 01:38:07 +0200

    Add openAPI security scheme
    Signed-off-by: Dušan Uverić <dusan.uveric@makit.lv>

---
+++ backend/src/main/kotlin/io/uvera/eobrazovanje/configuration/openapi/OpenAPIConfig.kt
@@ -0,0 +1,17 @@
+package io.uvera.eobrazovanje.configuration.openapi
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.security.SecurityScheme
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@OpenAPIDefinition(info = Info(title = "eObrazovanje API", version = "v1"))
+@SecurityScheme(
+    name = "bearerAuth",
+    type = SecuritySchemeType.HTTP,
+    bearerFormat = "JWT",
+    scheme = "bearer"
+)
+class OpenAPIConfig
