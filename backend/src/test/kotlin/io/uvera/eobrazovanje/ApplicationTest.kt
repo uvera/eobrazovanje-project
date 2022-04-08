efa63a70bdcea3df7576751a1458b9446a4445d5
Dušan Uverić <dusan.uveric@makit.lv>
2022-04-08 17:34:56 +0200

    Add entityWithAuth helper with ApplicationTest
    Signed-off-by: Dušan Uverić <dusan.uveric@makit.lv>

--- backend/src/test/kotlin/io/uvera/eobrazovanje/ApplicationTest.kt
+++ backend/src/test/kotlin/io/uvera/eobrazovanje/ApplicationTest.kt
@@ -6,20 +6,21 @@ import io.uvera.eobrazovanje.common.repository.UserRepository
 import io.uvera.eobrazovanje.security.configuration.CustomUserDetails
 import io.uvera.eobrazovanje.security.configuration.RoleEnum
 import io.uvera.eobrazovanje.security.service.JwtAccessService
-import org.junit.Before
+import liquibase.pro.packaged.T
+import liquibase.pro.packaged.it
 import org.junit.jupiter.api.BeforeEach
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
-import org.springframework.test.web.reactive.server.WebTestClient
-import org.springframework.test.web.servlet.MockHttpServletRequestDsl
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.util.LinkedMultiValueMap
 import org.springframework.web.context.WebApplicationContext
 import org.testcontainers.containers.PostgreSQLContainer
 import org.testcontainers.junit.jupiter.Container
@@ -38,6 +39,7 @@ abstract class ApplicationTest {
 
     @Autowired
     lateinit var studentRepository: StudentRepository
+
     @Autowired
     lateinit var subjectRepository: StudentRepository
 
@@ -90,5 +92,10 @@ abstract class ApplicationTest {
         }
     }
 
-    fun MockHttpServletRequestDsl.withAdminAuthorizationHeader() = this.header("Authorization", "Bearer $adminToken")
+    fun <T : Any?> entityWithAuth(body: T) = HttpEntity<T>(
+        body,
+        HttpHeaders().also {
+            it.add("Authorization", "Bearer $adminToken")
+        }
+    )
 }
