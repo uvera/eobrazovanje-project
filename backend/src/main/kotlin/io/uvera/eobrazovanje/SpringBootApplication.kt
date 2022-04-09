60c8e0fd3fdec2e61a356b90384c43c3816b850e
Dušan Uverić <dusan.uveric@makit.lv>
2022-04-09 10:43:16 +0200

    App: properly configure blaze persistence
    Signed-off-by: Dušan Uverić <dusan.uveric@makit.lv>

--- backend/src/main/kotlin/io/uvera/eobrazovanje/SpringBootApplication.kt
+++ backend/src/main/kotlin/io/uvera/eobrazovanje/SpringBootApplication.kt
@@ -1,19 +1,58 @@
 package io.uvera.eobrazovanje
 
+import com.blazebit.persistence.Criteria
+import com.blazebit.persistence.CriteriaBuilderFactory
 import com.blazebit.persistence.integration.view.spring.EnableEntityViews
+import com.blazebit.persistence.spi.CriteriaBuilderConfiguration
+import com.blazebit.persistence.view.EntityViewManager
+import com.blazebit.persistence.view.impl.EntityViewConfigurationImpl
+import com.blazebit.persistence.view.spi.EntityViewConfiguration
 import io.uvera.eobrazovanje.configuration.properties.ConfigurationPropertiesMarker
+import org.springframework.beans.factory.config.ConfigurableBeanFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Scope
 import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.scheduling.annotation.EnableScheduling
+import javax.persistence.EntityManagerFactory
+import javax.persistence.PersistenceUnit
+
 
 @SpringBootApplication
 @EnableAsync
 @EnableScheduling
 @EnableEntityViews(basePackages = ["io.uvera.eobrazovanje"])
 @ConfigurationPropertiesScan(basePackageClasses = [ConfigurationPropertiesMarker::class])
-class KotlinSpringBootApp
+class KotlinSpringBootApp {
+
+    @PersistenceUnit
+    lateinit var entityManagerFactory: EntityManagerFactory
+
+    @Bean
+    @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
+    @org.springframework.context.annotation.Lazy(false)
+    fun createCriteriaBuilderFactory(): CriteriaBuilderFactory {
+        val config: CriteriaBuilderConfiguration = Criteria.getDefault()
+        // do some configuration
+        return config.createCriteriaBuilderFactory(entityManagerFactory)
+    }
+
+    @Bean
+    @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
+    fun entityViewConfiguration() = EntityViewConfigurationImpl()
+
+    @Bean
+    @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
+    @org.springframework.context.annotation.Lazy(false)
+    fun createEntityViewManager(
+        cbf: CriteriaBuilderFactory,
+        entityViewConfiguration: EntityViewConfiguration,
+    ): EntityViewManager? {
+        return entityViewConfiguration.createEntityViewManager(cbf)
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<KotlinSpringBootApp>(*args)
