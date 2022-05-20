785e029a089d0ff8c2b6cfe1cd2c88ad1d47de98
Boris Jancic <jancicboris56@gmail.com>
2022-05-20 22:47:57 +0200

    Implemented basic profile overview for student

--- frontend/src/app/student/components/profile/profile.component.ts
+++ frontend/src/app/student/components/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CurrentStudentDTO, ProfileComponentService } from './profile.service';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  student: CurrentStudentDTO = {
+    id: '',
+    transcriptNumber: '',
+    identificationNumber: '',
+    user: {
+      firstName: '',
+      email: '',
+      lastName: ''
+    }
   }
 
+  constructor(
+    private readonly service: ProfileComponentService
+  ) { }
+
+  ngOnInit(): void {
+    this.fetchCurrentStudentInfo()
+  }
+
+  fetchCurrentStudentInfo() {
+    this.service.fetchCurrentUser()
+    .pipe()
+    .subscribe((res) => {
+      const responseBody = res?.body;
+      if (responseBody) {
+        const { email }  = responseBody;
+        this.service.
+        fetchCurrentStudent(email)
+          .subscribe((res) => {
+            const responseBody = res?.body
+            if (responseBody)
+              this.student = responseBody
+            console.log(this.student)
+          })
+        }
+      })
+    }
 }
