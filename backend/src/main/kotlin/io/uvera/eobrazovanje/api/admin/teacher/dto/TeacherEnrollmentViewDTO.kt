a12f97fc04d25d0d5fe6ffe19493d267984eb3bf
Ivan Djuraki <ivandjuraki@protonmail.com>
2022-05-25 15:17:11 +0200

    migration, fix teacher enrollment

--- backend/src/main/kotlin/io/uvera/eobrazovanje/api/admin/teacher/dto/TeacherEnrollmentViewDTO.kt
+++ backend/src/main/kotlin/io/uvera/eobrazovanje/api/admin/teacher/dto/TeacherEnrollmentViewDTO.kt
@@ -2,23 +2,24 @@ package io.uvera.eobrazovanje.api.admin.teacher.dto
 
 import com.blazebit.persistence.view.CollectionMapping
 import com.blazebit.persistence.view.EntityView
+import io.uvera.eobrazovanje.api.admin.student.dto.EnrollmentViewDTO
 import io.uvera.eobrazovanje.api.admin.subject.dto.SubjectViewDTO
+import io.uvera.eobrazovanje.api.admin.subjectExecution.dto.SubjectExecutionViewDTO
 import io.uvera.eobrazovanje.common.repository.Teacher
 import io.uvera.eobrazovanje.common.repository.SubjectExecution
+import io.uvera.eobrazovanje.common.repository.SubjectProfessorEnrollment
 import java.time.LocalDateTime
 import java.util.*
 
 @EntityView(Teacher::class)
 interface TeacherEnrollmentViewDTO {
     val id: UUID
-    val year: Int
     @get:CollectionMapping
-    val subjectExecution: List<SubjectExecutionViewDTO>
+    val subjectProfessorEnrollments: List<SubjectProfessorEnrollmentViewDTO>
 
-    @EntityView(SubjectExecution::class)
-    interface SubjectExecutionViewDTO {
-        val place: String
-        val time: LocalDateTime
-        val subject: SubjectViewDTO
+    @EntityView(SubjectProfessorEnrollment::class)
+    interface SubjectProfessorEnrollmentViewDTO {
+        val year: Int
+        val subjectExecution: EnrollmentViewDTO.SubjectExecutionViewDTO
     }
 }
