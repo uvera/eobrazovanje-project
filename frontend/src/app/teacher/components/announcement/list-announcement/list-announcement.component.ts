4ab7e84fddb16c27606088a55737121b00261402
Milan Dakic <fakultetdakic@gmail.com>
2022-06-02 18:14:42 +0200

    Announcement WIP

--- frontend/src/app/teacher/components/announcement/list-announcement/list-announcement.component.ts
+++ frontend/src/app/teacher/components/announcement/list-announcement/list-announcement.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit, Pipe, PipeTransform } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { NzTableQueryParams } from 'ng-zorro-antd/table';
+import { BehaviorSubject, combineLatest, first, map } from 'rxjs';
+import { ExamPeriodsViewDTO } from 'src/app/admin/components/exam-periods/list-exam-periods-tab/list-exam-periods-tab.component';
+import { StudentsViewDTO } from 'src/app/admin/components/students/list-students-tab/list-students-tab.component';
+import { SubjectExecutionViewDTO } from 'src/app/admin/components/subject-executions/edit-subject-executions-dialog/edit-subject-executions-dialog.component';
+import { AreYouSureDialogComponent } from 'src/app/common/components/dialogs/are-you-sure-dialog/are-you-sure-dialog.component';
+import { ListAnnouncementService } from './list-announcement.service';
 
 @Component({
   selector: 'app-list-announcement',
@@ -6,10 +17,95 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-announcement.component.scss']
 })
 export class ListAnnouncementComponent implements OnInit {
+  readonly pageIndex = new BehaviorSubject<number>(1);
+  readonly total = new BehaviorSubject<number>(1);
+  readonly pageSize = new BehaviorSubject<number>(10);
+  readonly dataSet = new BehaviorSubject<AnnouncementViewDTO[]>([]);
+  opSubjects: SubjectExecutionViewDTO[] = [];
+  form!: FormGroup;
+  teacherId: string = '';
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private service: ListAnnouncementService,
+    private snack: MatSnackBar,
+    private dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
+    this.form = this.fb.group({
+      id: [null],
+    });
+    this.service
+      .getAnnouncements()
+      .pipe(first())
+      .subscribe((res) => {
+        this.opSubjects = res?.body ?? [];
+      });
+    this.pageNumberAndSizeCombined$.subscribe((value) => {
+      this.fetchFromApi(
+        this.form.get('id')?.value,
+        value.pageIndex,
+        value.pageSize,
+      );
+    });
+    this.form.valueChanges.subscribe((x) => {
+      this.reloadFromApi();
+    });
   }
 
+  pageIndex$ = this.pageIndex.asObservable();
+  pageSize$ = this.pageSize.asObservable();
+  pageNumberAndSizeCombined$ = combineLatest([
+    this.pageIndex$,
+    this.pageSize$,
+  ]).pipe(
+    map((x) => ({
+      pageIndex: x[0],
+      pageSize: x[1],
+    }))
+  );
+
+  fetchFromApi(id: string, pageIndex: number, pageSize: number) {
+    if (id) {
+      this.service
+        .fetchPaged(id, pageIndex, pageSize)
+        .pipe(first())
+        .subscribe((res) => {
+          const responseBody = res?.body;
+          if (responseBody) {
+            const { content, totalElements } = responseBody;
+            console.log(content)
+            this.total.next(totalElements);
+            this.dataSet.next(content);
+          }
+        });
+    }
+  }
+
+  queryParamsChange(event: NzTableQueryParams) {
+    this.pageSize.next(event.pageSize);
+    this.pageIndex.next(event.pageIndex);
+  }
+
+  reloadFromApi() {
+    this.fetchFromApi(
+      this.form.get('id')?.value,
+      this.pageIndex.value,
+      this.pageSize.value,
+    );
+  }
+}
+
+export interface AnnouncementViewDTO {
+  id: String;
+  postText: String;
+  TeacherResponseDTO: {
+    id: string;
+    teacherType: string;
+    user: {
+    firstName: string;
+    lastName: string;
+    };
+  }
 }
--- frontend/src/app/teacher/components/announcement/list-announcement/list-announcement.service.ts
+++ frontend/src/app/teacher/components/announcement/list-announcement/list-announcement.service.ts
@@ -1,9 +1,50 @@
+import { HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ExamPeriodsViewDTO } from 'src/app/admin/components/exam-periods/list-exam-periods-tab/list-exam-periods-tab.component';
+import { PageEntity } from 'src/app/common/http/page-entity';
+import { ApiService } from 'src/app/common/service/api.service';
+import { CurrentTokenInfoDTO, CurrentTeacherDTO } from '../create-announcement/create-announcement.service';
+import { TeacherEnrollmentViewDTO } from '../create-announcement/create-announcement.component'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ListAnnouncementService {
+  constructor(private readonly api: ApiService) {}
 
-  constructor() { }
+  fetchPaged(id: string, pageIndex: number, pageSize: number) {
+    const params = new HttpParams()
+      .set('id', id)
+      .set('page', pageIndex)
+      .set('records', pageSize);
+    return this.api.get<PageEntity>('/api/admin/announcement', params);
+  }
+
+  getAnnouncements() {
+    return this.api.get<PageEntity[]>('/api/admin/announcement');
+  }
+
+  fetchCurrentUser() {
+    const params = new HttpParams();
+    return this.api.get<CurrentTokenInfoDTO>('/api/auth/whoami', params);
+  }
+
+  fetchCurrentTeacher(email: string) {
+    const params = new HttpParams().set('email', email);
+    return this.api.get<CurrentTeacherDTO>('/api/teacher/whoami', params);
+  }
+
+  getSubjectExecutions(id: string) {
+    return this.api.get<TeacherEnrollmentViewDTO[]>(
+      '/api/teacher/subjects-all', {id: id}
+    );
+  }
+
+  getSubjectExecutions2(id: string, pageIndex: number, pageSize: number) {
+    const params = new HttpParams()
+      .set('id', id)
+      .set('page', pageIndex)
+      .set('records', pageSize);
+    return this.api.get<PageEntity>('/api/admin/announcement', params);
+  }
 }
