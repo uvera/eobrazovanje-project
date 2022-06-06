94cb572b5566b22a8327076e273ce370881806f1
Dušan Uverić <dusan.uveric@makit.lv>
2022-06-06 21:42:58 +0200

    frontend: do not parse image url from body
    Signed-off-by: Dušan Uverić <dusan.uveric@makit.lv>

--- frontend/src/app/student/components/documents/list-documents-tab/list-documents-tab.component.ts
+++ frontend/src/app/student/components/documents/list-documents-tab/list-documents-tab.component.ts
@@ -11,34 +11,13 @@ export class ListDocumentsTabComponent implements OnInit {
   items: any = [];
   constructor(private readonly service: ListDocumentsTabService) {}
 
-  parseBody(value: any) {
-    const ret = value.map((i: { files: any[] }) => {
-      console.log(i);
-      return {
-        ...i,
-        files: [
-          ...i.files.map((f) => {
-            return {
-              ...f,
-              url: f.url
-                .replace('minio-db-lb', 'localhost')
-                .replace(/\?.*$/, ''),
-            };
-          }),
-        ],
-      };
-    });
-    console.log(ret);
-    return ret;
-  }
-
   ngOnInit(): void {
     this.service
       .getMine()
       .pipe(take(1))
       .subscribe({
         next: (v) => {
-          this.items = this.parseBody(v.body);
+          this.items = v.body;
         },
       });
   }
