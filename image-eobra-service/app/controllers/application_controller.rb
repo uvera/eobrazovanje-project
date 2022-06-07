c6f3dcbe206fdf87efb923bd6dcf152cf0146b14
Dušan Uverić <dusan.uveric@makit.lv>
2022-06-07 13:51:47 +0200

    Rails/Image: log auth response
    Signed-off-by: Dušan Uverić <dusan.uveric@makit.lv>

--- image-eobra-service/app/controllers/application_controller.rb
+++ image-eobra-service/app/controllers/application_controller.rb
@@ -2,11 +2,16 @@ class ApplicationController < ActionController::API
   handle_api_errors()
 
   def authorize_as_student
+    url = ENV['SPRING_APP_URL'] || 'http://localhost:8080'
+    token = request.headers.fetch('Authorization', '')
     conn = Faraday.new(
-      url: ENV['SPRING_APP_URL'] || 'http://localhost:8080',
-      headers: { 'Content-Type' => 'application/json', 'Authorization' => request.headers.fetch('Authorization', '') }
+      url: url,
+      headers: { 'Content-Type' => 'application/json', 'Authorization' => token }
     )
+    logger.info "Fetching from main API with url #{url}, token: #{token}"
     response = conn.get('/api/student/whoami')
+    logger.info "Got response status: #{response.status}"
+    logger.info "Got response body: #{response.body}"
     if response.status == 200
       @student = JSON.parse(response.body).deep_symbolize_keys
     end
